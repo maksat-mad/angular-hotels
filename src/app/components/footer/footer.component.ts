b42b83fa4ed50e9ca36e61ca35073f383423d7f2
Maksat <maksat.madeniyetov@gmail.com>
2023-04-13 15:40:12 +0600

    feat: footer style change

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  links = [
+    ['https://telegram.org/', 'assets/social-media-icons/vk.svg', 'vk'],
+    ['https://www.tiktok.com/', 'assets/social-media-icons/twitter.svg', 'twitter'],
+    ['https://www.instagram.com/', 'assets/social-media-icons/telegram.svg', 'telegram'],
+    ['https://www.whatsapp.com/', 'assets/social-media-icons/youtube.svg', 'youtube']
+  ];
 }
