cf6218b92e08d846500c92b49054a15d500eebbb
Maksat <maksat.madeniyetov@gmail.com>
2023-04-21 01:48:40 +0600

    feat: login page auth

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
-import {NgForm} from "@angular/forms";
+import {Component, inject} from '@angular/core';
 import {LoginInfo} from "../../../models/auth/AuthInfo";
+import {AuthService} from "../../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
   hide = true;
+  isLoading = false;
+  hasError = false;
 
   loginInfo: LoginInfo = {
     email: '',
     password: ''
   }
 
-  login(form: NgForm) {
-    console.log(form.value)
+  login() {
+    this.isLoading = true;
+    this.authService.login(this.loginInfo.email, this.loginInfo.password)
+      .then(() => this.router.navigate(['/hotels']))
+      .catch(() => {
+        this.hasError = true;
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
   }
 }
