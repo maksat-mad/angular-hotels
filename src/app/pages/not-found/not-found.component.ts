97ab720beb43c97f67e6ee440e09c64033c25eac
Maksat <maksat.madeniyetov@gmail.com>
2023-05-10 01:51:40 +0300

    feat: not-found page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  template: `
+      <div class="container">
+          <div class="d-flex flex-column justify-content-center align-items-center mt-5">
+              <h3 class="text-secondary">{{'No such page' | translate}}</h3>
+              <a routerLink="/"
+                 class="text-decoration-none text-secondary-red-on-hover">{{'Go to home page' | translate}}</a>
+          </div>
+      </div>  `
 })
 export class NotFoundComponent {
 
